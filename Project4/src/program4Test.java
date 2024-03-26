788c1eba9670ac847cbef5a18c647dd4f58d03d7
Romoor <74839554+Romoor@users.noreply.github.com>
2024-03-26 14:45:05 -0700

    Changes

--- Project4/src/program4Test.java
+++ Project4/src/program4Test.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class program4Test {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,7 +21,7 @@ public class program4Test {
             playGame();
 
         }
-
+        // lose();
     }
 
     /**
@@ -30,7 +31,7 @@ public class program4Test {
 
         Queue inst1 = new Queue();
         Queue inst2 = new Queue();
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
         boolean continueLoop = false;
         do {
             System.out.println("Queue Selection");
@@ -102,11 +103,11 @@ public class program4Test {
 
         } while (continueLoop);
         System.out.println("Goodbye!");
-        scanner.close();
+        // scanner.close();
     }
 
     private static void playGame() {
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
         /**
          * Create Deck
@@ -264,6 +265,8 @@ public class program4Test {
             System.out.println("Tied deck size of " + player1.size);
 
         }
+        // scanner.close();
+
     }
 
     private static void shuffle(GreekHero[] cards) {
@@ -277,11 +280,11 @@ public class program4Test {
     }
 
     private static String selectProperty(GreekHero card) {
-        Scanner scanner = new Scanner(System.in);
 
         // select property
         System.out.print("Select your property: ");
         String property = scanner.next();
+        // scanner.close();
         return property;
     }
 
@@ -310,4 +313,97 @@ public class program4Test {
         return value;
     }
 
+}
+
+class Queue {
+    final int ALLOC = 16;
+    Object[] Queue;
+    int top; // front index
+    int bottom;
+    int size;
+
+    public Queue() {
+        Queue = new Object[ALLOC];
+        top = 0;
+        bottom = 0;
+        size = 0;
+    }
+
+    public void newLength() {
+        // System.out.println("expand called");
+        Object[] newQueue = new Object[Queue.length * 2];
+        // System.out.println("size: " + size);
+        for (int i = 0; i < bottom; i++) {
+            newQueue[i] = Queue[i];
+            // System.out.println("queue[" + i + "]:" + newQueue[i]);
+        }
+        Queue = newQueue;
+    }
+
+    public Object delete() {
+        // System.out.println("size: " + size);
+        if (isEmpty()) {
+            return null;
+        }
+        // System.out.println("top: " + top);
+        Object first = Queue[0];
+        // System.out.println("Delete: " + first);
+        size--;
+
+        for (int i = 0; i < size; i++) {
+            Queue[i] = Queue[i + 1];
+        }
+
+        // System.out.println("First: " + Queue[0]);
+        bottom--;
+        // System.out.println("Last: " + Queue[bottom]);
+
+        return first;
+    }
+
+    public void insert(Object obj) {
+        size++;
+
+        if (bottom >= Queue.length) {
+            newLength();
+        }
+
+        Queue[bottom] = obj;
+        // System.out.println("Insert: " + obj);
+        // System.out.println("First: " + Queue[top]);
+        // System.out.println("Last: " + Queue[bottom]);
+        bottom++;
+
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public String toString() {
+        // System.out.println("Tostring size: " + size);
+        String string = "";
+        string += "Index       Value\n";
+        for (int i = 0; i < size; i++) {
+            string += " " + (i + 1) + "             " + Queue[i] + "\n";
+        }
+        return string;
+    }
+
+    public String toStore() {
+        String string = "";
+        // System.out.println("ToSTore size: " + Queue.length);
+
+        string += "Index       Value\n";
+        for (int i = 0; i < Queue.length; i++) {
+            Object value = 0;
+            if (Queue[i] == null)
+                value = "null";
+            else
+                value = Queue[i];
+            // System.out.println(value);
+            string += " " + i + "             " + value + "\n";
+        }
+        return string;
+    }
 }
