01dbfad8e37686d6f83c84c4e3ab228be75579a7
Romoor <74839554+Romoor@users.noreply.github.com>
2024-04-11 18:43:22 -0700

    Link practice

--- Program 5/src/LinkPractice.java
+++ Program 5/src/LinkPractice.java
@@ -2,25 +2,33 @@ class Link {
     public Object data;
     public Link next;
 
-    public Link(Object _data) {
-        data = _data;
-        next = null;
-    }
-
-    public Link(Object _data, Link _next) {
-        data = _data;
-        next = _next;
-    }
+    /*
+     * public Link(Object _data) {
+     * data = _data;
+     * next = null;
+     * }
+     * 
+     * public Link(Object _data, Link _next) {
+     * data = _data;
+     * next = _next;
+     * }
+     */
 
 }
 
 public class LinkPractice {
 
     public Object makeTwoLinks(Object x, Object y) {
-        Link newLinkY = new Link(x);
+        Link newLinkX = new Link();
+        Link newLinkY = new Link();
 
-        Link newLinkX = new Link(x, newLinkY);
-        return newLinkX.data;
+        newLinkX.data = x;
+        newLinkY.data = y;
+
+        newLinkX.next = newLinkY;
+        newLinkX.next = null;
+
+        return newLinkX;
     }
 
 }
